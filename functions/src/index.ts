519538f70e11449590012edc38b843e04b7dc058
mather <mather314@gmail.com>
2020-10-03 03:33:56 +0900

    Firebase init (no actual implementations).

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.app = functions.https.onRequest((req, res) => {
+  res.send("Hello, world!");
+});
