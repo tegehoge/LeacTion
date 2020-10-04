bf45385d962a2dfc960b29ac7c8bfdd7656091a5
mather <mather314@gmail.com>
2020-10-04 15:52:10 +0900

    Implement firebase cloudfunctions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,73 @@
 import * as functions from "firebase-functions";
+import * as firebase from "firebase-admin";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firebaseConfig = functions.config() ? functions.config().firebase : {};
 
-exports.app = functions.https.onRequest((req, res) => {
-  res.send("Hello, world!");
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+
+const app = express();
+const api = express.Router();
+
+app.use(express.json());
+
+api.get("/event/:id", (req, res) => {
+  const event_id = req.params["id"];
+  firestore
+    .doc(`events/${event_id}`)
+    .get()
+    .then((snapshot) => {
+      res.set("Cache-Control", "public, max-age=300, s-maxage=10");
+      res.send(snapshot.data());
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(404).send("");
+    });
 });
+
+api.post("/event", (req, res) => {
+  const event = req.body;
+
+  firestore
+    .doc(`events/${event.id}`)
+    .set(event)
+    .then(() => res.json({ message: "ok" }))
+    .catch((e) => {
+      console.error(e);
+      res.status(400);
+    });
+});
+
+api.get("/event/:id/comments", (req, res) => {
+  const event_id = req.params["id"];
+  const comments: object[] = [];
+  firestore
+    .collection(`comments-${event_id}`)
+    .stream()
+    .on("data", (snapshot) => {
+      comments.push(snapshot.data());
+    })
+    .on("end", () => {
+      res.set("Cache-Control", "public, max-age=5, s-maxage=2");
+      res.json(comments);
+    });
+});
+
+api.post("/event/:id/comment", (req, res) => {
+  const event_id = req.params["id"];
+  const comment = req.body as { id: string };
+  firestore
+    .doc(`comments-${event_id}/${comment.id}`)
+    .set(comment)
+    .then(() => res.json({ message: "ok" }))
+    .catch((e) => {
+      console.error(e);
+      res.status(400);
+    });
+});
+
+app.use("/api", api);
+
+exports.app = functions.https.onRequest(app);
