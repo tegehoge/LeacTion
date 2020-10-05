00136227b0737f187325c8243a6cf2798954d787
mather <mather314@gmail.com>
2020-10-06 04:02:46 +0900

    Update functions to verify password.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,14 @@
 import * as functions from "firebase-functions";
 import * as firebase from "firebase-admin";
 import * as express from "express";
+import * as bcrypt from "bcrypt";
+
+import {
+  EventRequestWithPassword,
+  EventPasswordRequest,
+  CommentRequest,
+  CommentLikeRequest,
+} from "./types";
 
 const firebaseConfig = functions.config() ? functions.config().firebase : {};
 
@@ -12,8 +20,8 @@ const api = express.Router();
 
 app.use(express.json());
 
-api.get("/event/:id", (req, res) => {
-  const event_id = req.params["id"];
+api.get("/event/:event_id", (req, res) => {
+  const event_id = req.params["event_id"];
   firestore
     .doc(`events/${event_id}`)
     .get()
@@ -23,25 +31,56 @@ api.get("/event/:id", (req, res) => {
     })
     .catch((e) => {
       console.error(e);
-      res.status(404).send("");
+      res.status(404);
+      res.json({ message: "not found" });
     });
 });
 
 api.post("/event", (req, res) => {
-  const event = req.body;
+  const eventWithPassword = req.body as EventRequestWithPassword;
+  const { password, ...event } = eventWithPassword;
+
+  const createNewEvent = (): Promise<any> => {
+    const passwordHashing = bcrypt.hash(password, 10);
+
+    const eventSaveTask = firestore.doc(`events/${event.id}`).set(event);
+    const passwordSaveTask = passwordHashing.then((hashedPassword) => {
+      return firestore.doc(`event-secrets/${event.id}`).set({ hashedPassword });
+    });
+
+    return Promise.all([eventSaveTask, passwordSaveTask]);
+  };
+  const updateEvent = (data: { hashedPassword: string }): Promise<any> => {
+    const { hashedPassword } = data;
+    return bcrypt.compare(password, hashedPassword).then((isVerified) => {
+      if (isVerified) {
+        return firestore.doc(`events/${event.id}`).update(event);
+      } else {
+        return Promise.reject("unauthorized");
+      }
+    });
+  };
 
   firestore
-    .doc(`events/${event.id}`)
-    .set(event)
+    .doc(`event-secrets/${event.id}`)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists) {
+        return updateEvent(snapshot.data() as { hashedPassword: string });
+      } else {
+        return createNewEvent();
+      }
+    })
     .then(() => res.json({ message: "ok" }))
     .catch((e) => {
       console.error(e);
       res.status(400);
+      res.json({ message: "failed" });
     });
 });
 
-api.get("/event/:id/comments", (req, res) => {
-  const event_id = req.params["id"];
+api.get("/event/:event_id/comments", (req, res) => {
+  const event_id = req.params["event_id"];
   const comments: object[] = [];
   firestore
     .collection(`comments-${event_id}`)
@@ -55,16 +94,66 @@ api.get("/event/:id/comments", (req, res) => {
     });
 });
 
-api.post("/event/:id/comment", (req, res) => {
-  const event_id = req.params["id"];
-  const comment = req.body as { id: string };
+api.post("/event/:event_id/verify", async (req, res) => {
+  const event_id = req.params["event_id"];
+  const { password } = req.body as EventPasswordRequest;
+
   firestore
-    .doc(`comments-${event_id}/${comment.id}`)
-    .set(comment)
-    .then(() => res.json({ message: "ok" }))
+    .doc(`event-secrets/${event_id}`)
+    .get()
+    .then((snapshot) => {
+      const { hashedPassword } = snapshot.data() as { hashedPassword: string };
+      return bcrypt.compare(password, hashedPassword);
+    })
+    .then((result) => {
+      if (result) {
+        res.json({ message: "ok" });
+      } else {
+        res.status(401).json({ message: "invalid password" });
+      }
+    })
     .catch((e) => {
       console.error(e);
       res.status(400);
+      res.json({ message: "failed" });
+    });
+});
+
+api.post("/event/:event_id/comment", (req, res) => {
+  const event_id = req.params["event_id"];
+  const comment = req.body as CommentRequest;
+  firestore
+    .doc(`comments-${event_id}/${comment.id}`)
+    .create(comment)
+    .then(() => res.json({ message: "ok" }))
+    .catch((e) => {
+      console.error(e);
+      res.status(404);
+      res.json({ message: "not found" });
+    });
+});
+
+api.post("/event/:event_id/comment/:comment_id/like", (req, res) => {
+  const event_id = req.params["event_id"];
+  const comment_id = req.params["comment_id"];
+  const likeReq = req.body as CommentLikeRequest;
+
+  const arrayUpdate = likeReq.remove
+    ? firebase.firestore.FieldValue.arrayRemove
+    : firebase.firestore.FieldValue.arrayUnion;
+
+  const docRef = firestore.doc(`comments-${event_id}/${comment_id}`);
+  docRef
+    .update({
+      likes: arrayUpdate(likeReq.user_id_hashed),
+    })
+    .then((result) => {
+      res.json({ message: "ok" });
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(400);
+      res.json({ message: "failed" });
     });
 });
 
---
+++ functions/src/types.d.ts
@@ -0,0 +1,33 @@
+type Talk = {
+  id: string;
+  title: string;
+  speaker_name: string;
+};
+export type EventRequest = {
+  id: string;
+  name: string;
+  date_of_event: string; // YYYY-MM-DD
+  talks: Talk[];
+  external_url?: string;
+};
+
+export type EventRequestWithPassword = EventRequest & EventPasswordRequest;
+
+export type EventPasswordRequest = {
+  password: string;
+};
+
+export type CommentRequest = {
+  id: string;
+  text: string;
+  user_id_hashed: string;
+  posted_at: string; // ISO 8601 format
+  event_id: string;
+  talk_id: string;
+  likes: string[];
+};
+
+export type CommentLikeRequest = {
+  user_id_hashed: string;
+  remove?: boolean;
+};
