749d49c3fc15a0c23f167e53dd2f2f38b5781592
mather <mather314@gmail.com>
2021-03-28 18:15:10 +0900

    Upgrade to Tailwindcss 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
     enabled: process.env.NODE_ENV == "production",
     content: ["./src/**/*.vue", "./src/**/*.html"],
