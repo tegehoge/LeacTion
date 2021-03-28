bdf9aacb2e1bc4c3cd6d4e055438f199383baace
mather <mather314@gmail.com>
2021-03-28 23:46:57 +0900

    Add link to github

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,13 @@
 <template>
   <header class="bg-blue-700 text-white">
-    <nav class="flex items-center p-1 md:p-3">
-      <div class="flex-shrink-0 pr-5">
+    <nav class="flex items-center justify-between p-1 md:p-3">
+      <div class="pr-5">
         <h1 class="text-3xl">LeacTion!</h1>
+      </div>
+      <div>
+        <a href="https://github.com/tegehoge/LeacTion" class="text-white"
+          ><font-awesome-icon :icon="['fab', 'github']" class="text-3xl"
+        /></a>
       </div>
     </nav>
   </header>
