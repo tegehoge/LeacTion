47fd25f1aa60e0e7b6dd205f279741dd2adfa893
mather <mather314@gmail.com>
2022-03-18 19:30:33 +0900

    Update NotFound page as vue 3.2

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,21 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
   <app-header></app-header>
   <div class="container m-auto">
     <h1 class="text-center m-5 text-xl">お探しのページは見つかりませんでした</h1>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import AppHeader from "../components/AppHeader.vue";
-
-export default defineComponent({
-  components: { AppHeader },
-  name: "NotFound",
-  setup(props, context) {
-    return {};
-  },
-});
-</script>
 
 <style></style>
