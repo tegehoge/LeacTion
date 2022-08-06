8f0dc4b564b716067099bb289877bd151a35132c
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2022-08-06 14:49:46 +0900

    [update]Appコンポーネントにルーティング処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import type { Component } from 'solid-js';
+import { Routes, Router, Route } from "@solidjs/router";
+import { Component, For } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { routes } from "~/constants/routes";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <For each={routes}>{(route) => <Route {...route} />}</For>
+      </Routes>
+    </Router>
   );
 };
 
