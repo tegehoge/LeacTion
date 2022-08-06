28b437be7477f964e0cb44cd32ec152a47e76e62
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2022-08-07 02:03:54 +0900

    [add]SUIDのthemeを追加

--- src/theme/createTheme.tsx
+++ src/theme/createTheme.tsx
@@ -3,7 +3,31 @@ import { createTheme as BaseCreateTheme } from "@suid/material/styles";
 export const createTheme = BaseCreateTheme({
   palette: {
     primary: {
-      main: "rgba(6, 108, 181, 1.0)",
+      light: "#dbeafe",
+      main: "#066cb5",
     },
+  },
+  breakpoints: {
+    values: {
+      "2sm": 768,
+    },
+  },
+  typography: {
+    fontFamily: [
+      "ui-sans-serif",
+      "system-ui",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Helvetica Neue",
+      "Arial",
+      "Noto Sans",
+      "sans-serif",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "Noto Color Emoji",
+    ].join(","),
   },
 });
