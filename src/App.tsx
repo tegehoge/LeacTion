55ca4a17f125facf8540fb5e23075b8b2b74ce4e
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2022-08-06 16:50:52 +0900

    [add]SUIDのthemaを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Routes, Router, Route } from "@solidjs/router";
+import { ThemeProvider } from "@suid/material/styles";
 import { Component, For } from "solid-js";
 
 import { routes } from "~/constants/routes";
+import { createTheme } from "~/theme/createTheme";
 
 const App: Component = () => {
   return (
-    <Router>
-      <Routes>
-        <For each={routes}>{(route) => <Route {...route} />}</For>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={createTheme}>
+      <Router>
+        <Routes>
+          <For each={routes}>{(route) => <Route {...route} />}</For>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme/createTheme.tsx
@@ -0,0 +1,9 @@
+import { createTheme as BaseCreateTheme } from "@suid/material/styles";
+
+export const createTheme = BaseCreateTheme({
+  palette: {
+    primary: {
+      main: "rgba(6, 108, 181, 1.0)",
+    },
+  },
+});
