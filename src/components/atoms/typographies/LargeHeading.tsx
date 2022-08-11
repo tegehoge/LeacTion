c6ad52f59ce7e4b8f644eb3b8a1ce03d87b044f6
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2022-08-11 22:51:41 +0900

    [add]タイトルで使用するHeadingコンポーネントの作成

---
+++ src/components/atoms/typographies/LargeHeading.tsx
@@ -0,0 +1,21 @@
+import { useTheme } from "@suid/material";
+import Typography from "@suid/material/Typography";
+import { Component, JSXElement, mergeProps } from "solid-js";
+
+export type Props = {
+  children: JSXElement;
+  color?: string;
+};
+
+const LargeHeading: Component<Props> = (props) => {
+  const theme = useTheme();
+  const merged = mergeProps({ color: theme.palette.grey["700"] }, props);
+
+  return (
+    <Typography variant="h5" fontWeight="bold" color={merged.color}>
+      {merged.children}
+    </Typography>
+  );
+};
+
+export default LargeHeading;
--- src/components/atoms/typographies/index.ts
+++ src/components/atoms/typographies/index.ts
@@ -1,2 +1,3 @@
+export { default as LargeHeading } from "./LargeHeading";
 export { default as LargeSizeText } from "./LargeSizeText";
 export { default as MediumSizeTextParagraph } from "./MediumSizeTextParagraph";
