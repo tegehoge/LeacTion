b32e7ae20ea80d9076a500029e81b96ef53d86b5
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-09-04 18:11:41 +0900

    [add]pageコンポーネントの追加

--- src/components/pages/EventNew.tsx
+++ src/components/pages/EventNew.tsx
@@ -1,46 +1,147 @@
 import { Link } from "@solidjs/router";
 import AddCircle from "@suid/icons-material/AddCircle";
-import Delete from "@suid/icons-material/Delete";
-import FormatLineSpacing from "@suid/icons-material/FormatLineSpacing";
 import TagIcon from "@suid/icons-material/Tag";
 import Box from "@suid/material/Box";
 import Container from "@suid/material/Container";
 import Divider from "@suid/material/Divider";
 import FormLabel from "@suid/material/FormLabel";
 import Grid from "@suid/material/Grid";
-import IconButton from "@suid/material/IconButton";
 import TextField from "@suid/material/TextField";
 import Typography from "@suid/material/Typography";
-import { red } from "@suid/material/colors";
-import { Component, createSignal, For } from "solid-js";
+import { dndzone as dndZoneDirective, TRIGGERS, SOURCES } from "solid-dnd-directive";
+import { Accessor, Component, createEffect, createSignal, For, JSX } from "solid-js";
+import { createStore, produce } from "solid-js/store";
 
-import { createStore } from "solid-js/store";
 import { PrimaryButton, SecondaryButton } from "~/components/atoms/buttons";
 import { CautionServiceUseModal } from "~/components/atoms/modals";
 import { useModal } from "~/components/atoms/modals/useModal";
 import { LargeHeading } from "~/components/atoms/typographies";
 import { EventNewMemberForm } from "~/components/organisms/events";
+import {
+  ConsiderEvent,
+  FinalizeEvent,
+  MouseDownEvent,
+  TouchStartEvent,
+} from "~/types/useCustomeDirective";
 
-const EventNew: Component = () => {
-  const { isOpen, onClose } = useModal(true);
-  const [date, setDate] = createSignal("2020-08-01");
-  const [events, setEvents] = createStore([
+export const [eventStore, setEventStore] = createStore({
+  name: "",
+  date: "",
+  url: "",
+  hashTag: "",
+  presentationList: [
     {
+      id: 1,
       memberName: "たなか",
       title: "Laravelについて",
     },
     {
+      id: 2,
       memberName: "すずき",
       title: "Ruby on Railsについて",
     },
     {
+      id: 3,
       memberName: "たかはし",
       title: "Next.jsについて",
     },
-  ]);
+  ],
+});
+
+const EventNew: Component = () => {
+  // @ref: https://github.com/isaacHagoel/solid-dnd-directive/issues/6
+  // @ref: https://codesandbox.io/s/dnd-drag-handles-57btm?file=/src/App.jsx
+  const dndzone = dndZoneDirective;
+  const { isOpen, onClose } = useModal(false);
+  const [date, setDate] = createSignal("2020-08-01");
+  const [dragDisabled, setDragDisabled] = createSignal(true);
+
+  // createEffect(() => {
+  //   console.log("dragDisabled", dragDisabled());
+  // });
+
+  // createEffect(() => {
+  //   console.log("eventStore title", eventStore[0].title);
+  // });
+  //
+  // createEffect(() => {
+  //   console.log("eventStore memberName", eventStore[0].memberName);
+  // });
+  // //
+  // createEffect(() => {
+  //   console.log("eventStore[0]", eventStore[0]);
+  // });
+  //
+  // createEffect(() => {
+  //   console.log("eventStore[0]", eventStore[1]);
+  // });
+  //
+  // createEffect(() => {
+  //   console.log("eventStore[0]", eventStore[2]);
+  // });
+  //
+  // const handleInputMemberNameEvent = (id: number, value: string): void => {
+  //   setEventStore(
+  //     (store) => store.id === id,
+  //     "memberName",
+  //     () => value
+  //   );
+  // };
+  //
+  // const handleInputTitleEvent = (id: number, value: string): void => {
+  //   setEventStore(
+  //     (store) => store.id === id,
+  //     "title",
+  //     () => value
+  //   );
+  // };
+
+  const handleInputEvent = (id: number, key: "title" | "memberName", value: string): void => {
+    setEventStore(
+      "presentationList",
+      (presentationList) => presentationList.id === id,
+      key,
+      () => value
+    );
+  };
 
   const addEvent = (): void => {
-    setEvents(() => [...events, { memberName: "", title: "" }]);
+    const nextId = Math.max(...eventStore.presentationList.map((event) => event.id)) + 1;
+
+    // setEventStore(eventStore.presentationList.length, { id: nextId, memberName: "", title: "" });
+  };
+
+  const handleConsider = (e: ConsiderEvent) => {
+    const {
+      items: newItems,
+      info: { source, trigger },
+    } = e.detail;
+
+    setEventStore(newItems);
+
+    // Ensure dragging is stopped on drag finish via keyboard
+    if (source === SOURCES.KEYBOARD && trigger === TRIGGERS.DRAG_STOPPED) {
+      setDragDisabled(true);
+    }
+  };
+
+  const handleFinalize = (e: FinalizeEvent) => {
+    const {
+      items: newItems,
+      info: { source },
+    } = e.detail;
+
+    setEventStore(newItems);
+    // Ensure dragging is stopped on drag finish via pointer (mouse, touch)
+    if (source === SOURCES.POINTER) {
+      setDragDisabled(true);
+    }
+  };
+
+  const startDrag = (e: MouseDownEvent | TouchStartEvent) => {
+    // preventing default to prevent lag on touch devices (because of the browser checking for screen scrolling)
+    e.preventDefault();
+    setDragDisabled(false);
   };
 
   return (
@@ -112,11 +213,25 @@ const EventNew: Component = () => {
             </Typography>
           </FormLabel>
 
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <For each={events}>
-              {(event) => <EventNewMemberForm title={event.title} memberName={event.memberName} />}
+          <div
+            style={{ display: "flex", "flex-direction": "column", gap: "16px" }}
+            use:dndzone={{ items: () => eventStore.presentationList, dragDisabled }}
+            on:consider={handleConsider}
+            on:finalize={handleFinalize}
+          >
+            <For each={eventStore.presentationList}>
+              {(event, index) => (
+                <EventNewMemberForm
+                  title={event.title}
+                  memberName={event.memberName}
+                  dragDisabled={dragDisabled()}
+                  startDrag={startDrag}
+                  id={event.id}
+                  handleInputEvent={handleInputEvent}
+                />
+              )}
             </For>
-          </Box>
+          </div>
         </Box>
 
         <Box marginBottom="16px">
