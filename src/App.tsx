979a30caa9814bc202ce702d3e3177b10a1f8789
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-11-09 01:14:09 +0900

    [add] tanstack-queryライブラリの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Router, useRoutes } from "@solidjs/router";
 import CssBaseline from "@suid/material/CssBaseline";
 import { ThemeProvider } from "@suid/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { Component } from "solid-js";
 
 import { routes } from "~/routes";
@@ -8,14 +9,17 @@ import { theme } from "~/theme";
 
 const App: Component = () => {
   const Routes = useRoutes(routes);
+  const queryClient = new QueryClient();
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Routes />
-      </Router>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
