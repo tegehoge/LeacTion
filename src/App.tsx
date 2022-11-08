3cbb4506f3425a2cfb93b3b2eba6e902b6d229eb
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-11-09 01:14:40 +0900

    [update] devツールの設定追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Router, useRoutes } from "@solidjs/router";
 import CssBaseline from "@suid/material/CssBaseline";
 import { ThemeProvider } from "@suid/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { attachDebugger } from "solid-devtools";
 import { Component } from "solid-js";
 
 import { routes } from "~/routes";
@@ -10,6 +11,7 @@ import { theme } from "~/theme";
 const App: Component = () => {
   const Routes = useRoutes(routes);
   const queryClient = new QueryClient();
+  attachDebugger();
 
   return (
     <QueryClientProvider client={queryClient}>
