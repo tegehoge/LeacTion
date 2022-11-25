8da5c758292ff26c97e6c3a3c598969c40deb185
mather <mather314@gmail.com>
2022-11-25 17:56:03 +0900

    Add Skeleton component on loading event administrator.
    管理者欄以外は auth の結果を待たなくて良いので局所化する

--- src/components/pages/EventNew.tsx
+++ src/components/pages/EventNew.tsx
@@ -6,12 +6,12 @@ import Divider from "@suid/material/Divider";
 import List from "@suid/material/List";
 import ListItem from "@suid/material/ListItem";
 import ListItemText from "@suid/material/ListItemText";
+import Skeleton from "@suid/material/Skeleton";
 import Typography from "@suid/material/Typography";
 import { createEffect, For, Match, Switch, VoidComponent } from "solid-js";
 
 import { PrimaryButton, SecondaryButton } from "~/components/atoms/buttons";
 import { LargeHeading } from "~/components/atoms/typographies";
-import { Loading } from "~/components/organisms/Loading";
 import { EventPresentationForm, EventInfoInputGroup } from "~/components/organisms/events";
 import { CautionServiceUseModal } from "~/components/organisms/modals";
 import { useAuthContext } from "~/firebase/AuthProvider";
@@ -43,88 +43,95 @@ const EventNew: VoidComponent = () => {
   });
 
   return (
-    <Switch>
-      <Match when={auth.loading}>
-        <Loading />
-      </Match>
-      <Match when={auth.account}>
-        <Box sx={{ textAlign: "center", margin: "20px 0" }}>
-          <LargeHeading>新規イベントを登録する</LargeHeading>
-        </Box>
+    <>
+      <Box sx={{ textAlign: "center", margin: "20px 0" }}>
+        <LargeHeading>新規イベントを登録する</LargeHeading>
+      </Box>
 
-        <Container maxWidth="lg">
-          <Box component={"div"}>
-            <Box marginBottom="24px">
-              <EventInfoInputGroup
-                onChange={onChangeEventInfo}
-                name={eventStore.name}
-                url={eventStore.url}
-                date={eventStore.date}
-                hashTag={eventStore.hashTag}
-              />
-            </Box>
+      <Container maxWidth="lg">
+        <Box component={"div"}>
+          <Box marginBottom="24px">
+            <EventInfoInputGroup
+              onChange={onChangeEventInfo}
+              name={eventStore.name}
+              url={eventStore.url}
+              date={eventStore.date}
+              hashTag={eventStore.hashTag}
+            />
+          </Box>
 
-            <Box marginBottom="16px">
-              <Typography variant="subtitle2" sx={{ fontWeight: "bold", marginBottom: "8px" }}>
-                イベントの発表順 (順序を変更できます)
-              </Typography>
-
-              <EventPresentationForm
-                handleInputEvent={onInputPresentationListItem}
-                presentationList={eventStore.presentationList}
-                setEventStore={setEventStore}
-              />
-            </Box>
-
-            <Box marginBottom="16px">
-              <SecondaryButton
-                onClick={onClickAddPresentationItem}
-                fullWidth={true}
-                startIcon={<AddCircle />}
-              >
-                発表枠の追加
-              </SecondaryButton>
-            </Box>
-
-            <Box marginBottom="16px">
-              <Divider />
-            </Box>
-
-            <Typography
-              variant="h6"
-              fontWeight="bold"
-              sx={{ textAlign: "center", marginBottom: "16px" }}
-            >
-              イベント管理者
+          <Box marginBottom="16px">
+            <Typography variant="subtitle2" sx={{ fontWeight: "bold", marginBottom: "8px" }}>
+              イベントの発表順 (順序を変更できます)
             </Typography>
 
-            <List>
-              <For each={eventStore.administrators}>
-                {(administrator, _) => (
-                  <ListItem>
-                    <ListItemText>{administrator.displayName}</ListItemText>
-                  </ListItem>
-                )}
-              </For>
-            </List>
-
-            <Box textAlign="center" marginBottom="32px">
-              <Typography>
-                <Link href="/terms" target="_blank">
-                  利用規約
-                </Link>
-                に同意して
-              </Typography>
-              <PrimaryButton onClick={() => console.log(eventStore)}>
-                イベントを作成する
-              </PrimaryButton>
-            </Box>
+            <EventPresentationForm
+              handleInputEvent={onInputPresentationListItem}
+              presentationList={eventStore.presentationList}
+              setEventStore={setEventStore}
+            />
           </Box>
-        </Container>
 
-        <CautionServiceUseModal open={isOpen()} onClose={() => onClose()} />
-      </Match>
-    </Switch>
+          <Box marginBottom="16px">
+            <SecondaryButton
+              onClick={onClickAddPresentationItem}
+              fullWidth={true}
+              startIcon={<AddCircle />}
+            >
+              発表枠の追加
+            </SecondaryButton>
+          </Box>
+
+          <Box marginBottom="16px">
+            <Divider />
+          </Box>
+
+          <Typography
+            variant="h6"
+            fontWeight="bold"
+            sx={{ textAlign: "center", marginBottom: "16px" }}
+          >
+            イベント管理者
+          </Typography>
+          <Switch>
+            <Match when={auth.loading}>
+              <List>
+                <ListItem>
+                  <ListItemText>
+                    <Skeleton variant="text" />
+                  </ListItemText>
+                </ListItem>
+              </List>
+            </Match>
+            <Match when={auth.account}>
+              <List>
+                <For each={eventStore.administrators}>
+                  {(administrator, _) => (
+                    <ListItem>
+                      <ListItemText>{administrator.displayName}</ListItemText>
+                    </ListItem>
+                  )}
+                </For>
+              </List>
+            </Match>
+          </Switch>
+
+          <Box textAlign="center" marginBottom="32px">
+            <Typography>
+              <Link href="/terms" target="_blank">
+                利用規約
+              </Link>
+              に同意して
+            </Typography>
+            <PrimaryButton onClick={() => console.log(eventStore)}>
+              イベントを作成する
+            </PrimaryButton>
+          </Box>
+        </Box>
+      </Container>
+
+      <CautionServiceUseModal open={isOpen()} onClose={() => onClose()} />
+    </>
   );
 };
 
