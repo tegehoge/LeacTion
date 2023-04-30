6dc84870833f57179600c26a5fd2478072881e6a
mather <mather314@gmail.com>
2023-05-01 01:28:26 +0900

    Implement link and new line in comment.

--- src/features/comment/components/SingleComment.tsx
+++ src/features/comment/components/SingleComment.tsx
@@ -1,6 +1,16 @@
-import { Box, Paper } from "@suid/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Link,
+  Paper,
+} from "@suid/material";
 import { Firestore } from "firebase/firestore";
-import { VoidComponent } from "solid-js";
+import { JSX, VoidComponent, createSignal, untrack } from "solid-js";
 
 import { Comment } from "../types";
 import { DeleteComment } from "./DeleteComment";
@@ -13,12 +23,44 @@ type SingleCommentProps = {
 };
 
 export const SingleComment: VoidComponent<SingleCommentProps> = (props) => {
+  const [linkUrl, setLinkUrl] = createSignal<string | null>(null);
+  const closeDialog = () => setLinkUrl(null);
+  const open = () => !!linkUrl();
+
+  const plainTextToHtml = (content: string): JSX.Element[] => {
+    const [first, ...rest] = content.split("\n").map((s) => <span>{s}</span>);
+    return rest.reduce<JSX.Element[]>((prev, curr) => prev.concat(<br />, curr), [first]);
+  };
+
+  const contentToHtml = (content: string) => {
+    const target = untrack(() => content);
+    const matches = target.matchAll(/https?:\/\/([\w-]+\.)+[\w:-]+(\/[\w./?%&#=~-]*)?/g);
+    const result: JSX.Element[] = [];
+    let cursor = 0;
+    for (const match of matches) {
+      console.log(match, cursor, match.index);
+      if (match.index !== undefined) {
+        if (cursor !== match.index) {
+          result.push(...plainTextToHtml(target.slice(cursor, match.index)));
+        }
+        const url = match[0];
+        result.push(
+          <Link onClick={() => setLinkUrl(url)} sx={{ cursor: "pointer" }}>
+            {url}
+          </Link>
+        );
+        cursor = match.index + url.length;
+      }
+    }
+    result.push(...plainTextToHtml(target.slice(cursor)));
+    return result;
+  };
   return (
     <Paper
       sx={{ display: "flex", padding: ["0.3rem", "0.5rem"], alignItems: "flex-start" }}
       title={props.comment.postedAt.toLocaleString()}
     >
-      <Box flexGrow={1}>{props.comment.content}</Box>
+      <Box flexGrow={1}>{contentToHtml(props.comment.content)}</Box>
       <Box sx={{ marginTop: "-5px", marginBottom: "-5px" }}>
         <DeleteComment
           firestore={props.firestore}
@@ -33,6 +75,29 @@ export const SingleComment: VoidComponent<SingleCommentProps> = (props) => {
           currentUid={props.currentUid}
         />
       </Box>
+      <Dialog open={open()} maxWidth="lg" onBackdropClick={closeDialog}>
+        <DialogTitle>URLの確認</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            このURLを開こうとしています。問題ありませんか？
+            <br />
+            {linkUrl()}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>キャンセル</Button>
+          <Button
+            variant="outlined"
+            color="warning"
+            component="a"
+            href={linkUrl()}
+            target="_blank"
+            onClick={closeDialog}
+          >
+            リンクを開く
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Paper>
   );
 };
