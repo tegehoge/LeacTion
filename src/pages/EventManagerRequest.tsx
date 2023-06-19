63e2d2e52c8cb7af224a74d561afea0d71cfaab4
mather <mather314@gmail.com>
2023-06-20 01:36:38 +0900

    Refine manager-request messages.

--- src/pages/EventManagerRequest.tsx
+++ src/pages/EventManagerRequest.tsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate, useParams } from "@solidjs/router";
-import { Box, Button, CircularProgress, Typography } from "@suid/material";
+import { Box, Button, CircularProgress, Container, Typography } from "@suid/material";
 import { Match, Show, Switch, VoidComponent, createEffect, createResource } from "solid-js";
 import { getEvent } from "~/features/event/api";
 import { createEventManagerRequests } from "~/features/event/api/createEventManagerRequest";
@@ -53,39 +53,51 @@ const EventManagerRequest: VoidComponent = () => {
   });
 
   return (
-    <Box textAlign="center">
+    <Container>
       <Show when={isManager() === false} fallback={<CircularProgress />}>
         <Switch>
-          <Match when={isManagerRequested()}>
-            <Box sx={{ margin: "20px" }}>
-              <Typography variant="h6">管理者リクエストを送信しました</Typography>
+          <Match when={!isManagerRequested()}>
+            <Box sx={{ margin: "20px", textAlign: "center" }}>
+              <Typography variant="h4">共同管理者リクエストを送る</Typography>
             </Box>
             <Box>
-              管理者リクエストが行われたことは現在の管理者へ通知されませんので、イベントの管理者に直接ご連絡ください。
-            </Box>
-            <Box sx={{ margin: "20px" }}>
-              <NavLink href={`/event/${eventId}`}>イベントページに戻る</NavLink>
-            </Box>
-          </Match>
-          <Match when={!isManagerRequested()}>
-            <Box sx={{ margin: "20px" }}>
-              <Typography variant="h6">管理者リクエスト</Typography>
+              共同管理者は次のことができます。
+              <ul>
+                <li>イベント名や発表枠の編集</li>
+                <li>他の共同管理者リクエストの承認・拒否</li>
+                <li>イベントの削除</li>
+              </ul>
             </Box>
             <Box>
               <Typography>
-                <span>{auth.account?.displayName}</span> としてイベント 「
-                <span>{event()?.name}</span>」 の管理権限をリクエストしますか？
+                現在の表示名「<span>{auth.account?.displayName}</span>」でイベント 「
+                <span>{event()?.name}</span>」 の共同管理者への追加をリクエストしますか？
+                <br />
+                （アカウントの表示名は右上のボタンから変更できます。他の管理者がわかりやすい名前にしてください。）
               </Typography>
             </Box>
-            <Box sx={{ margin: "20px" }}>
+            <Box sx={{ margin: "20px", textAlign: "center" }}>
               <Button variant="contained" color="primary" onClick={sendRequest}>
-                リクエストする
+                共同管理者への追加をリクエストする
               </Button>
+            </Box>
+          </Match>
+          <Match when={isManagerRequested()}>
+            <Box sx={{ margin: "20px", textAlign: "center" }}>
+              <Typography variant="h4">共同管理者リクエストは送信済みです</Typography>
+            </Box>
+            <Box>
+              共同管理者リクエストが行われたことは、本サービスからは現在の管理者へ通知されません。
+              <br />
+              現在の管理者に承認いただくようにご自身で直接ご連絡ください。
+            </Box>
+            <Box sx={{ margin: "20px", textAlign: "center" }}>
+              <NavLink href={`/event/${eventId}`}>イベントページに戻る</NavLink>
             </Box>
           </Match>
         </Switch>
       </Show>
-    </Box>
+    </Container>
   );
 };
 
